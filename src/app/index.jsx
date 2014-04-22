7294b84bbc56a53adb94a93e4470ff956ef84ffe
Aetetic <aetetic@gmail.com>
2014-04-22 22:38:35 +0400

    Initial commit

---
+++ src/app/index.jsx
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var Index = React.createClass({
+  render: function () {
+    return (
+      <div className="index">Hack, it!</div>
+    );
+  }
+});
+React.renderComponent(
+    <Index /> , document.querySelector('.appl'));
