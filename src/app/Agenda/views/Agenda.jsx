99974a891a3a88151bd3d8c2675fb02716178c3d
Aetetic <aetetic@gmail.com>
2014-04-30 21:02:33 +0400

    Comments for agenda view

--- src/app/Agenda/views/Agenda.jsx
+++ src/app/Agenda/views/Agenda.jsx
@@ -5,11 +5,19 @@ var React = require('react'),
     Toolbar = require('root/Toolbar/views/Toolbar'),
     Content = require('root/Content/views/Content');
 var Agenda = React.createClass({
+  //Подгружаются события на один месяц.
+  //Если текущая дата меньше 10, то подгружаем прошлый месяц и текущий
+  //Если текущая дата больше 20, то подгружаем текущий и следующий
+  //Если между 10 и 20 подгружаем только текущий
+  //Каждая подгрузка увеличивает количество объектов в state
+  //Или оставляет его неизменным
   getInitialState: function () {
     return {
       events: [{
         date: '01.05.03',
         title: 'Hello'
+      }, {
+        date: ''
       }]
     };
   },
